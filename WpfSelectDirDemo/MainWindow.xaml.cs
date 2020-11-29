b930c7f64490671e206f272ae0db4af7f177fe78
= <brenordv@users.noreply.github.com>
2020-11-29 01:54:01 -0300

    Initial Commit

---
+++ WpfSelectDirDemo/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace WpfSelectDirDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            using var dialog = new CommonOpenFileDialog {IsFolderPicker = true};
+
+            if (dialog.ShowDialog() != CommonFileDialogResult.Ok)
+            {
+                TxtLabel.Content = "User Canceled";
+                return;
+            }
+
+            TxtLabel.Content = dialog.FileName;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Folder picker Dialog Demo
+This is a simple demo of an application show how to use a system dialog to select a folder.
